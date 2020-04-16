1788d66ba5e8403f1e0c225f67e7bc0145fc1e18
Chance Strickland <hi@chancedigital.io>
2020-04-15 20:50:35 -0700

    update favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,6 +86,15 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-plugin-use-dark-mode',
+      // options: {
+      //   classNameDark: 'dark-mode',
+      //   classNameLight: 'light-mode',
+      //   storageKey: 'darkMode',
+      //   minify: true,
+      // },
+    },
+    {
       resolve: 'gatsby-plugin-sass',
       options: {
         data: `@import "${__dirname}/src/lib/styles/utils";`,
